7065daf0023c7a75b11d3aed12999c4c04064c3e
Trinh-Quoc-Trong <rtytgb2423@gmail.com>
2025-02-25 06:58:39 +0700

    Initial commit

---
+++ test001.py
@@ -0,0 +1,6 @@
+a = r"""Last summer, my family and I had a wonderful vacation at the beach. We traveled to a beautiful coastal city and stayed in a small hotel near the sea. Every morning, we watched the sunrise and took a walk along the shore. During the day, we swam in the ocean, built sandcastles, and enjoyed delicious seafood. In the evening, we explored the night market and bought some souvenirs. The weather was perfect, and we had so much fun together. It was a memorable trip, and I hope we can go on another vacation like this in the future.
+
+"""
+a = a.split(' ')
+print(a)
+print(f"so luong chu da viet {len(a)}")
